c1452c5c2dff833b0148668f3f51e2967212b93c
seika-afk <artistnoob6@gmail.com>
2026-05-18 02:11:03 +0530

    feat(Frontend):Added Sidebar:searchbar:playbar

--- Frontend/analogflix/app/page.tsx
+++ Frontend/analogflix/app/page.tsx
@@ -1,21 +1,60 @@
 "use client";
 
 import SidePlayer from "@/components/SidePlayer";
+import { Birdhouse, Cable, Link, Search, X } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const handleSearch = () => {
+    console.log("Searched");
+  };
+
   return (
     <div className="flex flex-row h-screen overflow-hidden">
-      <div className="flex-5 flex flex-row bg-red-500">
+      <div className="flex-5 flex flex-row ">
         {/* side bar */}
-        <div className="bg-yellow-300 h-screen w-19 border-t-3 border-r-3 border-black"></div>
+        <div className="bg-[#383838] h-screen w-19 border-t-3 border-r-3 border-black">
+          <div className="flex mt-7   justify-center items-center cursor-pointer hover:scale-110 transition-all text-[#252525] duration-200 hover:text-[#B8B8B8]">
+            <Birdhouse onClick={() => router.push("/")} size={30} />
+          </div>
+          <div className="flex mt-7 justify-center items-center cursor-pointer hover:scale-110 transition-all text-[#252525] duration-200 hover:text-[#B8B8B8]">
+            <Cable
+              onClick={() =>
+                (window.location.href =
+                  "https://github.com/seika-afk/AnalogFlix")
+              }
+              size={30}
+            />
+          </div>
+
+          <div className="flex mt-7 justify-center items-center cursor-pointer hover:scale-110 transition-all text-[#252525] duration-200 hover:text-[#B8B8B8]">
+            <X
+              onClick={() => (window.location.href = "https://x.com/srrw2s")}
+              size={30}
+            />
+          </div>
+        </div>
         {/*top bar // search bar*/}
         <div className="flex flex-col w-full">
-          <div className="bg-green-400 w-full h-20  border-t-3 border-b-3 border-black"></div>
-          <div className="bg-pink-200 w-full flex-1  border-black border-b-3"></div>{" "}
-          {/* flex-1 fills remaining */}
+          <div className="bg-[#383838] overflow-hidden flex justify-start items-center w-full h-20  border-t-3 border-b-3 border-black">
+            <div className="bg-[#7d7d7d] flex items-center gap-2 ml-5  h-10 w-[50%] border-black border-3  px-4">
+              <Search size={18} className="text-[#252525] shrink-0" />
+              <input
+                onKeyDown={(e) => {
+                  if (e.key == "Enter") {
+                    handleSearch();
+                  }
+                }}
+                placeholder="Title, author, host, or topic"
+                className="bg-transparent outline-none border-none text-[#252525] placeholder-[#252525] w-full text-sm"
+              />
+            </div>
+          </div>
+          <div className="bg-[#383838] w-full flex-1  border-black border-b-3"></div>{" "}
         </div>
       </div>
-      <div className="bg-blue-500  flex-2  border-l-3 border-r-3 border-t-3 border-b-3 border-black">
+      <div className=" flex-2  bg-[#383838] border-l-3 border-r-3 border-t-3 border-b-3 border-black">
         <SidePlayer />
       </div>
     </div>
--- Frontend/analogflix/components/SidePlayer.tsx
+++ Frontend/analogflix/components/SidePlayer.tsx
@@ -1,9 +1,13 @@
+import { Ghost, GhostIcon, Play } from "lucide-react";
+
 const SidePlayer = () => {
   return (
     <div className="flex flex-col items-center">
       {/* Top bar */}
-      <div className="bg-green-400 w-full h-19.5 border-b-3 border-black flex items-center px-6">
-        LIMINAL
+      <div className="bg-[#383838] text-[#b8b8b8] flex items-center justify-center w-full h-19.5 border-b-3 border-black flex items-center px-6">
+        <Ghost size={30} />
+
+        <p className="text-3xl font-bold"> LIMINAL </p>
       </div>
 
       {/* Content */}
@@ -15,12 +19,13 @@ const SidePlayer = () => {
           />
         </div>
 
-        <button className="w-[320px] mt-3 py-3 bg-orange-500 text-white font-semibold flex items-center justify-center gap-2 rounded-sm hover:bg-orange-600 transition">
-          ▶ Play Sample
+        <button className=" cursor-pointer w-[320px] mt-3 py-3 bg-[#252525] text-white font-semibold flex items-center justify-center gap-2  hover:bg-[#B8B8B8] hover:text-[#383838] transition">
+          <Play size={20} />
+          Play Sample
         </button>
 
-        <div className="w-[320px] mt-2 flex border border-gray-300 rounded-sm">
-          <button className="flex-1 flex flex-col items-center justify-center py-3 gap-1 text-xs font-medium hover:bg-gray-100 transition border-r border-gray-300">
+        <div className="w-[320px] mt-2 flex border border-gray-300 ">
+          <button className=" cursor-pointer flex-1 flex flex-col items-center justify-center py-3 gap-1 text-xs font-medium hover:bg-[#B8B8B8] hover:text-[#383838] transition border-r border-gray-300">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-5 h-5"
@@ -37,7 +42,7 @@ const SidePlayer = () => {
             </svg>
             Channel
           </button>
-          <button className="flex-1 flex flex-col items-center justify-center py-3 gap-1 text-xs font-medium hover:bg-gray-100 transition">
+          <button className=" cursor-pointer flex-1 flex flex-col items-center justify-center py-3 gap-1 text-xs font-medium hover:bg-[#B8B8B8] hover:text-[#383838] transition">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-5 h-5"
@@ -59,10 +64,10 @@ const SidePlayer = () => {
         <hr className="w-[320px] mt-4 border-gray-300" />
 
         <div className="w-[320px] mt-3">
-          <h3 className="text-sm font-semibold text-gray-800 mb-1">
+          <h1 className="text-md font-semibold text-[#b8b8b8] mb-1">
             Description
-          </h3>
-          <p className="text-sm text-gray-600 leading-relaxed">
+          </h1>
+          <p className="text-sm text-[#A3A3A3] leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
